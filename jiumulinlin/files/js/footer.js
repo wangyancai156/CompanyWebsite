aa7b9eee57c2ef636e3384cab151286362c14ad3
wangyancai666 <ycai@jk-sci.com>
2021-03-25 11:47:20 +0800

    更新样式

--- jiumulinlin/files/js/footer.js
+++ jiumulinlin/files/js/footer.js
@@ -2,8 +2,8 @@ document.writeln(`
 <div class="footer">
 <div class="container">
     <div class="row">
-        <div class="col-md-3 col-sm-4"> <a href="http://demoall.dedecms58.com/42935" class="logo"><img
-                    src="./files/logo-light.png" alt=""></a> </div>
+        <div class="col-md-4 col-sm-4"> <a href="http://demoall.dedecms58.com/42935" class="logo"><img
+                    src="./files/logo.png" alt=""></a> </div>
         <div class="col-md-2 col-sm-8 hidden-xs">
             <h6>案例中心</h6>
             <ul>
@@ -24,17 +24,17 @@ document.writeln(`
         </div>
         <div class="col-md-4 col-sm-12">
             <h6>联系方式</h6>
-            <p>地址：广东省广州市番禺经济开发区</p>
-            <a href="http://demoall.dedecms58.com/42935/" class="btn"><i class="fa fa-phone"></i>
-                电话：020-88888888</a>
+            <p>地址：山东曹县庄寨镇工业园</p>
+            <a href="tel:13665309999" class="btn"><i class="fa fa-phone"></i>
+                电话：13665309999</a>
         </div>
     </div>
     <div class="row">
         <div class="col-sm-12">
             <hr>
         </div>
-        <div class="col-sm-6">Copyright © 2012-2020 某某网络科技有限公司 版权所有 <a
-                href="http://www.beian.miit.gov.cn/">备案号：粤ICP备xxxxxxxx号</a></div>
+        <div class="col-sm-6">Copyright © 2012-2020 优木拓科技有限公司 版权所有 <a
+                href="http://www.beian.miit.gov.cn/">备案号：京ICP备18005879</a></div>
         <div class="col-sm-6 hidden-xs">
 
         </div>
