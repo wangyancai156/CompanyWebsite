197c6e28e73b416625ce67919dfbf385591f0e00
wangyancai666 <ycai@jk-sci.com>
2021-03-25 08:00:41 +0800

    修改样式

---
+++ jiumulinlin/files/js/footer.js
@@ -0,0 +1,116 @@
+document.writeln(` <div class="space text-center">
+<div class="container"> 如果您有什么问题，欢迎咨询技术员 <a
+        href="http://wpa.qq.com/msgrd?v=3&amp;uin=88888888&amp;site=qq&amp;menu=yes" class="btn">点击QQ咨询</a>
+</div>
+</div>
+
+<div class="footer">
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-4"> <a href="http://demoall.dedecms58.com/42935" class="logo"><img
+                    src="./files/logo-light.png" alt=""></a> </div>
+        <div class="col-md-2 col-sm-8 hidden-xs">
+            <h6>案例中心</h6>
+            <ul>
+
+            </ul>
+        </div>
+        <div class="col-md-2 col-sm-8 hidden-xs">
+            <h6>案例中心</h6>
+            <ul>
+
+                <li> <a href="http://demoall.dedecms58.com/gongsidongtai/">公司动态</a></li>
+
+                <li> <a href="http://demoall.dedecms58.com/wangzhanzhishi/">网站知识</a></li>
+
+                <li> <a href="http://demoall.dedecms58.com/wangzhanyunying/">网站运营</a></li>
+
+            </ul>
+        </div>
+        <div class="col-md-4 col-sm-12">
+            <h6>联系方式</h6>
+            <p>地址：广东省广州市番禺经济开发区</p>
+            <a href="http://demoall.dedecms58.com/42935/" class="btn"><i class="fa fa-phone"></i>
+                电话：020-88888888</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <hr>
+        </div>
+        <div class="col-sm-6">Copyright © 2012-2020 某某网络科技有限公司 版权所有 <a
+                href="http://www.beian.miit.gov.cn/">备案号：粤ICP备xxxxxxxx号</a></div>
+        <div class="col-sm-6 hidden-xs">
+
+        </div>
+    </div>
+</div>
+</div>
+<div class="search-pop"> <a href="javascript:;" class="close fa fa-times-circle"></a>
+<div class="content" style="height: 100%;">
+    <form action="http://demoall.dedecms58.com/42935/%3Cyhcms:searchurl%20/%3E/" method="post"
+        style="display: table;height: 100%;width: 100%;">
+        <div style="display: table-cell;vertical-align: middle;">
+            <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 text-center">
+                <div class="ipt"> <i class="fa fa-search search-button" type="submit"></i>
+
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+</div>
+<div class="cndns-right hidden-xs">
+<div class="cndns-right-meau meau-sev"> <a href="javascript:" class="cndns-right-btn" style="border: none;">
+        <span class="fa fa-qq"></span>
+        <p>QQ咨询</p>
+    </a>
+    <div class="cndns-right-box">
+        <div class="box-border">
+            <div class="sev-t"> <span class="fa fa-comments"></span>
+                <p>QQ在线咨询<i>真诚为您提供专业解答服务</i></p>
+                <div class="clear"></div>
+            </div>
+            <div class="sev-b">
+                <ul class="clearfix">
+                    <li><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=88888888&amp;site=qq&amp;menu=yes">售前技术</a>
+                    </li>
+                    <li><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=88888888&amp;site=qq&amp;menu=yes">售前技术</a>
+                    </li>
+                    <li><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=88888888&amp;site=qq&amp;menu=yes">售后技术</a>
+                    </li>
+                    <li><a href="http://wpa.qq.com/msgrd?v=3&amp;uin=88888888&amp;site=qq&amp;menu=yes">售后技术</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cndns-right-meau meau-contact"> <a href="javascript:" class="cndns-right-btn"> <span
+            class="fa fa-phone-square"></span>
+        <p> 咨询热线 </p>
+    </a>
+    <div class="cndns-right-box">
+        <div class="box-border">
+            <div class="sev-t clearfix"> <span class="fa fa-phone-square"></span>
+                <p>020-88888888<br>
+                    <i>全天候技术服务热线</i>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cndns-right-meau meau-code"> <a href="javascript:" class="cndns-right-btn"> <span
+            class="fa fa-wechat"></span>
+        <p>添加微信</p>
+    </a>
+    <div class="cndns-right-box">
+        <div class="box-border">
+            <div class="sev-t"><img src="./files/weixin.jpg" alt="二维码"><i>微信便捷交流</i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cndns-right-meau meau-top" id="top-back" style="display: none;"> <a href="javascript:"
+        class="cndns-right-btn"> <span class="fa fa-angle-double-up"></span> <i>返回顶部</i> </a> </div>
+</div>`)
---
+++ jiumulinlin/files/js/header.js
@@ -0,0 +1,30 @@
+document.writeln(` 
+<div class="nav-bar clearfix">
+    <div class="nav-mobile-toggle"> <i class="fa fa-bars"></i> </div>
+    <div class="nav-logo">
+        <div class="pc_logo"> <a href="http://demoall.dedecms58.com/42935"> <img alt="某某网络科技有限公司"
+                    class="logo logo-light" src="./files/logo.png"> </a> </div>
+        <div class="wap_logo"> <a href="http://demoall.dedecms58.com/42935"> <img src="./files/logo@2x.png">
+            </a> </div>
+    </div>
+    <div class="nav-menu">
+        <ul class="menu clearfix">
+            <li><a href="index.html">网站首页</a></li>
+
+            <li><a href="about.html">公司简介</a></li>
+
+            <li><a href="chanpinzhongxin.html">案例中心</a></li>
+
+            <li><a href="news.html">新闻动态</a></li>
+
+            <li><a href="http://demoall.dedecms58.com/42935/zixunzhongxin/">资讯中心</a></li>
+
+            <li><a href="http://demoall.dedecms58.com/42935/zaixianliuyan/">在线留言</a></li>
+
+            <li><a href="http://demoall.dedecms58.com/42935/lianxiwomen/">联系我们</a></li>
+
+            <li><a href="http://demoall.dedecms58.com/42935/sitemap.html">网站地图</a></li>
+        </ul>
+    </div>
+</div> 
+`)
